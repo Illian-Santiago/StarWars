f8bd74718bab8eae81b1c1c96e0954ee17a3854b
Illian-Santiago <illianclase12345678@gmail.com>
2024-12-02 16:51:26 +0000

    Revert "Mejora logica sin terminar main.js"
    This reverts commit ebcb22d91f1165e9bc6017814851bb4057c0ab9d.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-function loadCharacters() {
-    $('main').empty();
-    $('main').append('<div></div>');
+$('#cambiarMain').on("click", loadCharacters);
 
+function loadCharacters() {
     if ($('#cambiarMain')[0].innerText == "Personajes") {
         let counter = 0;
 
+        $('main').empty();
+
+        $('main').append('<div></div>');
         $('main').find("div").addClass('container')
 
         for (let i = 1; i <= 88; i++) {
@@ -23,23 +25,6 @@ function loadCharacters() {
             $('#cambiarMain').text('Cargar Películas');
         }
     } else {
-        const movies = [
-            { title: "Ascenso Skywalker", image: "/resources/AscensoSkywalker.jpg" },
-            { title: "Imperio Contraataca", image: "/resources/ImperioContraataca.jpg" },
-            { title: "Últimos Jedi", image: "/resources/UltimosJedi.jpg" }
-        ]
-
-        $('main').find("div").addClass('imagenes-pelis grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8')
-
-        movies.forEach((movie) => {
-            $('main').find("div").append(`
-                <div class="flex justify-center">
-                    <img src=`+ movie.image + `
-                    class="rounded-lg shadow-xl transform hover:scale-105 transition-all duration-300">
-                </div>
-            `);
-        });
-
-        $('#cambiarMain').text('Cargar Personajes');
+        location.reload();
     }
 }
